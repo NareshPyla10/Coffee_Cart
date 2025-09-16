f04fb8fe28e8369f9bbb321b3fc7d027192baeda
NareshPyla10 <nareshpyla10@gmail.com>
2025-09-16 11:03:05 +0530

    Update mvn command

--- src/test/java/Runner/MenuRunner.java
+++ src/test/java/Runner/MenuRunner.java
@@ -7,8 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(
-        features = "classpath:features",  // ✅ better than hardcoding src/test/resources
-        glue = "StepDef",                              // ✅ your step definition package
+        features = "classpath:features",
+        glue = "StepDef",
         plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true
 )
